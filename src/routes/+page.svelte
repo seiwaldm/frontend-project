ed1fda7e5d8caf36199151a81d2cabb805278ce1
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-11-19 10:52:34 +0100

    added frontend-mentor-project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button class="btn">DaisyUI</button>
+<a class="btn text-red-500" href="https://daisyui.com/" target="_blank">DaisyUI</a>
