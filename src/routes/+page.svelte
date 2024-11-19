9d209c357d1714c52cb92552ac43c5396079a8f1
Leonit <begzati.leonit@johak.at>
2024-11-19 11:03:51 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 <h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a class="btn text-red-500" href="https://daisyui.com/" target="_blank">DaisyUI</a>
+<button class="btn">DaisyUI</button>
+<a class="btn text-red-500" href="https://daisyui.com">DaisyUI</a>
+
+<h1>Hallo Herr Fessa!</h1>
